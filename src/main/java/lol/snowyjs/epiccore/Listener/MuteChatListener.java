3ff17062abbde61de4aa8b0db8c811eff891a9a9
snowy <133208096+snowy-cool@users.noreply.github.com>
2024-06-09 19:04:09 +0100

    MuteChat Chat Listener

---
+++ src/main/java/lol/snowyjs/epiccore/Listener/MuteChatListener.java
@@ -0,0 +1,28 @@
+package lol.snowyjs.epiccore.Listener;
+
+import lol.snowyjs.epiccore.DataStorage.yaml.Lang;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+public class MuteChatListener implements Listener {
+
+    private final Lang lang;
+    private static boolean chatMuted = false;
+
+    public MuteChatListener(Lang lang) {
+        this.lang = lang;
+    }
+
+    public static void setChatMuted(boolean muted) {
+        chatMuted = muted;
+    }
+
+    @EventHandler
+    public void onPlayerChat(AsyncPlayerChatEvent event) {
+        if (chatMuted && !event.getPlayer().hasPermission("epiccore.mutechat.bypass")) {
+            event.getPlayer().sendMessage(lang.getReplacedMessage("chat-muted-message"));
+            event.setCancelled(true);
+        }
+    }
+}
