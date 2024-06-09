5cc7c9da37fe50fd0d555592dd9e8163c375e9a5
snowy <133208096+snowy-cool@users.noreply.github.com>
2024-06-09 17:42:37 +0100

    Discord Command

--- src/main/java/lol/snowyjs/epiccore/Commands/DiscordCommand.java
+++ src/main/java/lol/snowyjs/epiccore/Commands/DiscordCommand.java
@@ -1,4 +1,38 @@
 package lol.snowyjs.epiccore.Commands;
 
-public class DiscordCommand {
+import lol.snowyjs.epiccore.DataStorage.yaml.Lang;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DiscordCommand implements CommandExecutor {
+
+    private final Lang lang;
+    private final JavaPlugin plugin;
+
+    public DiscordCommand(JavaPlugin plugin, Lang lang) {
+        this.plugin = plugin;
+        this.lang = lang;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("discord")) {
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+
+                if (player.hasPermission("epiccore.discord")) {
+                    player.sendMessage(lang.getReplacedMessage("discord-link"));
+                } else {
+                    player.sendMessage(lang.getReplacedMessage("no-permission"));
+                }
+            } else {
+                sender.sendMessage("This command can only be executed by a player.");
+            }
+            return true;
+        }
+        return false;
+    }
 }
