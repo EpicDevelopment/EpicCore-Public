e876e459c62da08b7aea0911ca8dc6274d1f9a6b
Awel <87609387+Invisgg@users.noreply.github.com>
2024-06-11 13:35:53 -0500

    Updated spawn file

--- src/main/java/gg/minecrush/epiccore/Commands/SetSpawnCommand.java
+++ src/main/java/gg/minecrush/epiccore/Commands/SetSpawnCommand.java
@@ -29,7 +29,12 @@ public class SetSpawnCommand implements CommandExecutor {
 
                 if (player.hasPermission(config.getValue("setspawn-command-permission"))) {
                     Location location = player.getLocation();
-                    plugin.getConfig().set("spawn.location", location);
+                    plugin.getConfig().set("spawn.location.word", location.getWorld().getName());
+                    plugin.getConfig().set("spawn.location.x", location.getX());
+                    plugin.getConfig().set("spawn.location.y", location.getY());
+                    plugin.getConfig().set("spawn.location.z", location.getZ());
+                    plugin.getConfig().set("spawn.location.pitch", location.getPitch());
+                    plugin.getConfig().set("spawn.location.yaw", location.getYaw());
                     plugin.saveConfig();
                     player.sendMessage(lang.getReplacedMessage("spawn-set"));
                 } else {
