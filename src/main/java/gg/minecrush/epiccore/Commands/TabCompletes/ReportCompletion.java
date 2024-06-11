0a10679e91253f2b65c8ad3817767ca421ef7295
Awel <87609387+Invisgg@users.noreply.github.com>
2024-06-11 13:23:34 -0500

    Updated spawn file

--- src/main/java/gg/minecrush/epiccore/Commands/TabCompletes/ReportCompletion.java
+++ src/main/java/gg/minecrush/epiccore/Commands/TabCompletes/ReportCompletion.java
@@ -1,4 +1,24 @@
 package gg.minecrush.epiccore.Commands.TabCompletes;
 
-public class ReportCompletion {
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReportCompletion implements TabCompleter {
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        List<String> completions = new ArrayList<>();
+        if (args.length == 1) {
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                completions.add(player.getName());
+            }
+            return completions;
+        }
+        return null;
+    }
 }
