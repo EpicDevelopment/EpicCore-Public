c49fd96134b8522f51391b3a4c14edb9e74d80b0
Awel <87609387+Invisgg@users.noreply.github.com>
2024-06-11 13:23:28 -0500

    Updated spawn file

--- src/main/java/gg/minecrush/epiccore/Commands/TabCompletes/ClearChatCompletion.java
+++ src/main/java/gg/minecrush/epiccore/Commands/TabCompletes/ClearChatCompletion.java
@@ -1,4 +1,20 @@
 package gg.minecrush.epiccore.Commands.TabCompletes;
 
-public class ClearChatCompletion {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClearChatCompletion implements TabCompleter {
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        List<String> completions = new ArrayList<>();
+        if (args.length == 1) {
+            completions.add("<lines>");
+            return completions;
+        }
+        return null;
+    }
 }
