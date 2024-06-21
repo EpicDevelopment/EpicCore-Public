bdccd6fc6f4618f17656db49f78c71806d5899aa
Awel <87609387+Invisgg@users.noreply.github.com>
2024-06-21 01:09:51 -0500

    v1

---
+++ src/main/java/gg/minecrush/epiccore/Commands/TabCompletes/GamemodeCompletion.java
@@ -0,0 +1,24 @@
+package gg.minecrush.epiccore.Commands.TabCompletes;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GamemodeCompletion implements TabCompleter {
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        List<String> completions = new ArrayList<>();
+        if (args.length == 1) {
+            completions.add("creative");
+            completions.add("survival");
+            completions.add("spectator");
+            completions.add("adventure");
+            return completions;
+
+        }
+        return null;
+    }
+}
