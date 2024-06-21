d590831e0893f6e0f208ca2253efb099d9265258
Awel <87609387+Invisgg@users.noreply.github.com>
2024-06-21 01:37:51 -0500

    v1

--- src/main/java/gg/minecrush/epiccore/Commands/gamemodes/gamemode.java
+++ src/main/java/gg/minecrush/epiccore/Commands/gamemodes/gamemode.java
@@ -3,6 +3,7 @@ package gg.minecrush.epiccore.Commands.gamemodes;
 import gg.minecrush.epiccore.DataStorage.yaml.Config;
 import gg.minecrush.epiccore.DataStorage.yaml.Lang;
 import gg.minecrush.epiccore.Util.color;
+import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -22,7 +23,7 @@ public class gamemode implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player) {
-            if (args.length != 1){
+            if (args.length == 0 || args.length < 2){
                 sender.sendMessage(lang.getReplacedMessage("invalid-arguments").replace("%invalid-arguments%", "/gamemode"));
                 return false;
             }
@@ -32,32 +33,43 @@ public class gamemode implements CommandExecutor {
                 return false;
             }
 
+            Player target = null;
+            if (args.length == 2){
+                target = Bukkit.getPlayer(args[1]);
+                if (target == null){
+                    sender.sendMessage(lang.getReplacedMessage("invalid-player"));
+                    return false;
+                }
+            } else {
+                target = (Player) sender;
+            }
+
             Player player = (Player) sender;
             String subCommand = args[0];
             switch (subCommand) {
                 case "creative":
-                    gamemodeCreative(player);
+                    gamemodeCreative(player, target);
                     break;
                 case "c":
-                    gamemodeCreative(player);
+                    gamemodeCreative(player, target);
                     break;
                 case "survival":
-                    gamemodeSurvival(player);
+                    gamemodeSurvival(player, target);
                     break;
                 case "s":
-                    gamemodeSurvival(player);
+                    gamemodeSurvival(player, target);
                     break;
                 case "adventure":
-                    gamemodeAdventure(player);
+                    gamemodeAdventure(player, target);
                     break;
                 case "a":
-                    gamemodeAdventure(player);
+                    gamemodeAdventure(player, target);
                     break;
                 case "spectator":
-                    gamemodeSpectator(player);
+                    gamemodeSpectator(player, target);
                     break;
                 case "sp":
-                    gamemodeSpectator(player);
+                    gamemodeSpectator(player, target);
                     break;
             }
         } else {
@@ -66,23 +78,23 @@ public class gamemode implements CommandExecutor {
         return true;
     }
 
-    private void gamemodeAdventure(Player player){
-        player.sendMessage(lang.getReplacedMessage("updated-gamemode").replace("%gamemode%", "adventure"));
-        player.setGameMode(GameMode.ADVENTURE);
+    private void gamemodeAdventure(Player player, Player target){
+        player.sendMessage(lang.getReplacedMessage("updated-gamemode").replace("%gamemode%", "adventure").replace("%player%", target.getName()));
+        target.setGameMode(GameMode.ADVENTURE);
     }
 
-    private void gamemodeSurvival(Player player){
-        player.sendMessage(lang.getReplacedMessage("updated-gamemode").replace("%gamemode%", "survival"));
-        player.setGameMode(GameMode.SURVIVAL);
+    private void gamemodeSurvival(Player player, Player target){
+        player.sendMessage(lang.getReplacedMessage("updated-gamemode").replace("%gamemode%", "survival").replace("%player%", target.getName()));
+        target.setGameMode(GameMode.SURVIVAL);
     }
 
-    private void gamemodeCreative(Player player){
-        player.sendMessage(lang.getReplacedMessage("updated-gamemode").replace("%gamemode%", "creative"));
-        player.setGameMode(GameMode.CREATIVE);
+    private void gamemodeCreative(Player player, Player target){
+        player.sendMessage(lang.getReplacedMessage("updated-gamemode").replace("%gamemode%", "creative").replace("%player%", target.getName()));
+        target.setGameMode(GameMode.CREATIVE);
     }
 
-    private void gamemodeSpectator(Player player){
-        player.sendMessage(lang.getReplacedMessage("updated-gamemode").replace("%gamemode%", "spectator"));
-        player.setGameMode(GameMode.SPECTATOR);
+    private void gamemodeSpectator(Player player, Player target){
+        player.sendMessage(lang.getReplacedMessage("updated-gamemode").replace("%gamemode%", "spectator").replace("%player%", target.getName()));
+        target.setGameMode(GameMode.SPECTATOR);
     }
 }
