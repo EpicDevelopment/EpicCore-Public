1cda7dba48d89fe183db80ef53f3ee1606b54195
Awel <87609387+Invisgg@users.noreply.github.com>
2024-06-21 01:54:24 -0500

    v1

---
+++ src/main/java/gg/minecrush/epiccore/Commands/TabCompletes/FlyCompletion.java
@@ -0,0 +1,25 @@
+package gg.minecrush.epiccore.Commands.TabCompletes;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FlyCompletion implements TabCompleter {
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        List<String> completions = new ArrayList<>();
+        if (args.length == 1) {
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                completions.add(player.getName());
+            }
+            return completions;
+        }
+        return null;
+    }
+
+}
