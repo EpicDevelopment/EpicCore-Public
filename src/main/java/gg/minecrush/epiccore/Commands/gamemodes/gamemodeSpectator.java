4b67f1f01def6c07eba907b750282c0742ac968c
Awel <87609387+Invisgg@users.noreply.github.com>
2024-06-21 00:32:57 -0500

    Updated spawn file

---
+++ src/main/java/gg/minecrush/epiccore/Commands/gamemodes/gamemodeSpectator.java
@@ -0,0 +1,42 @@
+package gg.minecrush.epiccore.Commands.gamemodes;
+
+import gg.minecrush.epiccore.DataStorage.yaml.Config;
+import gg.minecrush.epiccore.DataStorage.yaml.Lang;
+import gg.minecrush.epiccore.Util.color;
+import org.bukkit.GameMode;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class gamemodeSpectator implements CommandExecutor {
+    Lang lang;
+    Config config;
+
+    public gamemodeSpectator(Lang lang, Config config) {
+        this.lang = lang;
+        this.config = config;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player) {
+
+            if (!sender.hasPermission(config.getValue("gmsp-command-permission"))){
+                sender.sendMessage(lang.getReplacedMessage("no-permission"));
+                return false;
+            }
+
+            Player player = (Player) sender;
+            if (args.length == 0) {
+                player.sendMessage(lang.getReplacedMessage("updated-gamemode").replace("%gamemode%", "spectator"));
+                player.setGameMode(GameMode.SPECTATOR);
+            } else {
+                player.sendMessage(lang.getReplacedMessage("invalid-arguments").replace("%invalid-arguments%", "/gmsp"));
+            }
+        } else {
+            sender.sendMessage(color.c("&cConsole cannot execute this command!"));
+        }
+        return false;
+    }
+}
