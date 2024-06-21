0bb5abc9c97228f0d45d06ceb68335b1ed44e58d
Awel <87609387+Invisgg@users.noreply.github.com>
2024-06-21 01:10:20 -0500

    v1

--- src/main/java/gg/minecrush/epiccore/Commands/MuteChatCommand.java
+++ src/main/java/gg/minecrush/epiccore/Commands/MuteChatCommand.java
@@ -8,6 +8,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MuteChatCommand implements CommandExecutor {
