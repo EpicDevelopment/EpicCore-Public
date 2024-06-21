ce2f541b39b4da0e7870297fac2cc90a572a6982
Awel <87609387+Invisgg@users.noreply.github.com>
2024-06-21 01:10:02 -0500

    v1

--- src/main/java/gg/minecrush/epiccore/Commands/gamemodes/gamemodeSpectator.java
+++ src/main/java/gg/minecrush/epiccore/Commands/gamemodes/gamemodeSpectator.java
@@ -37,6 +37,6 @@ public class gamemodeSpectator implements CommandExecutor {
         } else {
             sender.sendMessage(color.c("&cConsole cannot execute this command!"));
         }
-        return false;
+        return true;
     }
 }
