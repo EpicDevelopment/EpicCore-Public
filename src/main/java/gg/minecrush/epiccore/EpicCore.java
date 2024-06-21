1e7844f00cff72896c7b6e6ea23242d376f07758
Awel <87609387+Invisgg@users.noreply.github.com>
2024-06-21 00:33:12 -0500

    v1

--- src/main/java/gg/minecrush/epiccore/EpicCore.java
+++ src/main/java/gg/minecrush/epiccore/EpicCore.java
@@ -6,6 +6,10 @@ import gg.minecrush.epiccore.Commands.*;
 import gg.minecrush.epiccore.Commands.TabCompletes.AdminCompletion;
 import gg.minecrush.epiccore.Commands.TabCompletes.ClearChatCompletion;
 import gg.minecrush.epiccore.Commands.TabCompletes.ReportCompletion;
+import gg.minecrush.epiccore.Commands.gamemodes.gamemode;
+import gg.minecrush.epiccore.Commands.gamemodes.gamemodeAdventure;
+import gg.minecrush.epiccore.Commands.gamemodes.gamemodeCreative;
+import gg.minecrush.epiccore.Commands.gamemodes.gamemodeSpectator;
 import gg.minecrush.epiccore.DataStorage.ram.ChatManager;
 import gg.minecrush.epiccore.DataStorage.yaml.Config;
 import gg.minecrush.epiccore.DataStorage.yaml.Filter;
@@ -99,6 +103,13 @@ public final class EpicCore extends JavaPlugin {
         getCommand("report").setTabCompleter(new ReportCompletion());
         getCommand("broadcast").setExecutor(new BroadcastCommand(lang, config));
 
+        getCommand("gma").setExecutor(new gamemodeAdventure(lang, config));
+        getCommand("gmc").setExecutor(new gamemodeCreative(lang, config));
+        getCommand("gmsp").setExecutor(new gamemodeSpectator(lang, config));
+        getCommand("gms").setExecutor(new gamemodeAdventure(lang, config));
+        getCommand("gamemode").setExecutor(new gamemode(lang, config));
+
+
         // Event Registers
 
         getServer().getPluginManager().registerEvents(new MuteChatListener(lang, chatManager, config), this);
@@ -114,6 +125,12 @@ public final class EpicCore extends JavaPlugin {
         registerPermission(config.getValue("mutechat-command-permission"), "Mutechat command", PermissionDefault.OP);
         registerPermission(config.getValue("setspawn-command-permission"), "Update server spawn", PermissionDefault.OP);
         registerPermission(config.getValue("discord-command-permission"), "Find the discord!", PermissionDefault.TRUE);
+        registerPermission(config.getValue("gma-command-permission"), "Update player gamemode", PermissionDefault.OP);
+        registerPermission(config.getValue("gmc-command-permission"), "Update player gamemode", PermissionDefault.OP);
+        registerPermission(config.getValue("gms-command-permission"), "Update player gamemode", PermissionDefault.OP);
+        registerPermission(config.getValue("gmsp-command-permission"), "Update player gamemode", PermissionDefault.OP);
+        registerPermission(config.getValue("gamemode-command-permission"), "Update player gamemode", PermissionDefault.OP);
+        registerPermission(config.getValue("fly-command-permission"), "Update player fly mode", PermissionDefault.OP);
 
         // Automatic Events Register
 
@@ -127,8 +144,26 @@ public final class EpicCore extends JavaPlugin {
         Bukkit.getPluginManager().addPermission(permission);
     }
 
+    public void unregisterPermission(String name, String description, PermissionDefault defaultValue){
+        Permission permission = new Permission(name, description, defaultValue);
+        Bukkit.getPluginManager().removePermission(permission);
+    }
+
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        unregisterPermission(config.getValue("staff-permission"), "Staff commands!", PermissionDefault.OP);
+        unregisterPermission(config.getValue("clearchat-permission"), "Clear the public chat!", PermissionDefault.OP);
+        unregisterPermission(config.getValue("chat-muted-bypass-permission"), "Bypass muted chat", PermissionDefault.OP);
+        unregisterPermission(config.getValue("admin-command-permission"), "Manage the core plugin", PermissionDefault.OP);
+        unregisterPermission(config.getValue("broadcast-command-permission"), "Broadcast messages", PermissionDefault.OP);
+        unregisterPermission(config.getValue("mutechat-command-permission"), "Mutechat command", PermissionDefault.OP);
+        unregisterPermission(config.getValue("setspawn-command-permission"), "Update server spawn", PermissionDefault.OP);
+        unregisterPermission(config.getValue("discord-command-permission"), "Find the discord!", PermissionDefault.TRUE);
+        unregisterPermission(config.getValue("gma-command-permission"), "Update player gamemode", PermissionDefault.OP);
+        unregisterPermission(config.getValue("gmc-command-permission"), "Update player gamemode", PermissionDefault.OP);
+        unregisterPermission(config.getValue("gms-command-permission"), "Update player gamemode", PermissionDefault.OP);
+        unregisterPermission(config.getValue("gmsp-command-permission"), "Update player gamemode", PermissionDefault.OP);
+        unregisterPermission(config.getValue("gamemode-command-permission"), "Update player gamemode", PermissionDefault.OP);
+        unregisterPermission(config.getValue("fly-command-permission"), "Update player fly mode", PermissionDefault.OP);
     }
 }
