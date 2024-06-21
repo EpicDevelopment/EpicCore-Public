e9dfb752cd1da82ed0a04adfe1faeca7dc4a4a34
Awel <87609387+Invisgg@users.noreply.github.com>
2024-06-21 01:54:27 -0500

    v1

---
+++ src/main/java/gg/minecrush/epiccore/Commands/TabCompletes/FlySpeedCompletion.java
@@ -0,0 +1,25 @@
+package gg.minecrush.epiccore.Commands.TabCompletes;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FlySpeedCompletion implements TabCompleter {
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        List<String> completions = new ArrayList<>();
+        if (args.length == 1) {
+            completions.add("<speed>");
+            completions.add("<player>");
+            return completions;
+        }
+
+        if (args.length == 2) {
+            completions.add("<speed>");
+        }
+        return null;
+    }
+}
