7184170ba1a01839d93601ed31b5e308e157bee9
Awel <87609387+Invisgg@users.noreply.github.com>
2024-06-22 17:39:31 -0500

    v1

--- src/main/java/gg/minecrush/epiccore/Commands/teleportation/spawn/SetSpawnCommand.java
+++ src/main/java/gg/minecrush/epiccore/Commands/teleportation/spawn/SetSpawnCommand.java
@@ -1,7 +1,8 @@
-package gg.minecrush.epiccore.Commands.spawn;
+package gg.minecrush.epiccore.Commands.teleportation.spawn;
 
 import gg.minecrush.epiccore.DataStorage.yaml.Config;
 import gg.minecrush.epiccore.DataStorage.yaml.Lang;
+import gg.minecrush.epiccore.DataStorage.yaml.Warps;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -14,11 +15,13 @@ public class SetSpawnCommand implements CommandExecutor {
     private final Lang lang;
     private final JavaPlugin plugin;
     private final Config config;
+    private final Warps warps;
 
-    public SetSpawnCommand(JavaPlugin plugin, Lang lang, Config config) {
+    public SetSpawnCommand(JavaPlugin plugin, Lang lang, Config config, Warps warps) {
         this.plugin = plugin;
         this.lang = lang;
         this.config = config;
+        this.warps = warps;
     }
 
     @Override
@@ -29,13 +32,13 @@ public class SetSpawnCommand implements CommandExecutor {
 
                 if (player.hasPermission(config.getValue("setspawn-command-permission"))) {
                     Location location = player.getLocation();
-                    plugin.getConfig().set("spawn.location.world", location.getWorld().getName());
-                    plugin.getConfig().set("spawn.location.x", location.getX());
-                    plugin.getConfig().set("spawn.location.y", location.getY());
-                    plugin.getConfig().set("spawn.location.z", location.getZ());
-                    plugin.getConfig().set("spawn.location.pitch", location.getPitch());
-                    plugin.getConfig().set("spawn.location.yaw", location.getYaw());
-                    plugin.saveConfig();
+
+                    warps.setValue("spawn.location.world", location.getWorld().getName());
+                    warps.setDoubble("spawn.location.x", location.getX());
+                    warps.setDoubble("spawn.location.y", location.getY());
+                    warps.setDoubble("spawn.location.z", location.getZ());
+                    warps.setFloat("spawn.location.pitch", location.getPitch());
+                    warps.setFloat("spawn.location.yaw", location.getYaw());
                     player.sendMessage(lang.getReplacedMessage("spawn-set"));
                 } else {
                     player.sendMessage(lang.getReplacedMessage("no-permission"));
