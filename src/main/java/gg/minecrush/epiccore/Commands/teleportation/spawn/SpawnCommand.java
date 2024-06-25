57c49e5df4dbbdc16156d2e0af8afd5ceb6b5863
Awel <87609387+Invisgg@users.noreply.github.com>
2024-06-25 15:37:48 -0500

    v1.1 dev

--- src/main/java/gg/minecrush/epiccore/Commands/teleportation/spawn/SpawnCommand.java
+++ src/main/java/gg/minecrush/epiccore/Commands/teleportation/spawn/SpawnCommand.java
@@ -1,8 +1,10 @@
 package gg.minecrush.epiccore.Commands.teleportation.spawn;
 
+import gg.minecrush.epiccore.DataStorage.ram.WarpManager;
 import gg.minecrush.epiccore.DataStorage.yaml.Config;
 import gg.minecrush.epiccore.DataStorage.yaml.Lang;
 import gg.minecrush.epiccore.DataStorage.yaml.Warps;
+import gg.minecrush.epiccore.Util.propermessage;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
@@ -10,6 +12,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -17,16 +20,20 @@ import java.util.Objects;
 
 public class SpawnCommand implements CommandExecutor {
 
-    private final Lang lang;
-    private final JavaPlugin plugin;
     private final Config config;
+    private final Lang lang;
     private final Warps warps;
+    private final Plugin plugin;
+    private final gg.minecrush.epiccore.Util.propermessage propermessage;
+    private final WarpManager warpManager;
 
-    public SpawnCommand(JavaPlugin plugin, Lang lang, Config config, Warps warps) {
-        this.plugin = plugin;
+    public SpawnCommand(Lang lang, Warps warps, Config config, JavaPlugin plugin, propermessage msg, WarpManager warpManager) {
         this.lang = lang;
-        this.config = config;
         this.warps = warps;
+        this.config = config;
+        this.plugin = plugin;
+        this.propermessage = msg;
+        this.warpManager = warpManager;
     }
 
     @Override
@@ -46,20 +53,32 @@ public class SpawnCommand implements CommandExecutor {
                     return false;
                 }
 
+                if (warpManager.isWarping(player.getUniqueId())){
+                    player.sendMessage(lang.getReplacedMessage("already-teleporting"));
+                    return false;
+                }
+
                 new BukkitRunnable() {
                     int count = 0;
 
                     @Override
                     public void run() {
                         if (count < config.getValueInt("teleport-delay")) {
-                            player.sendMessage(lang.getReplacedMessage("warp-teleporting-in-seconds").replace("%warp%", "spawn").replace("%delay%", 5 - count + ""));
+                            propermessage.send(player, lang.getReplacedMessage("warp-teleporting-in-seconds").replace("%warp%", "spawn").replace("%delay%", 5 - count + ""));
+                            //player.sendMessage(lang.getReplacedMessage("warp-teleporting-in-seconds").replace("%warp%", "spawn").replace("%delay%", 5 - count + ""));
                             count++;
                             if (!Objects.equals(formatWithString(loc), formatWithString(player.getLocation()))) {
-                                player.sendMessage(lang.getReplacedMessage("warp-teleporting-canceled").replace("%warp%", "spawn"));
+                                propermessage.send(player, lang.getReplacedMessage("warp-teleporting-canceled").replace("%warp%", "spawn"));
+                                //player.sendMessage(lang.getReplacedMessage("warp-teleporting-canceled").replace("%warp%", "spawn"));
+                                cancel();
+                            }
+                            if (!warpManager.isWarping(player.getUniqueId())){
+                                propermessage.send(player, lang.getReplacedMessage("warp-teleporting-canceled").replace("%warp%", args[0]));
                                 cancel();
                             }
                         } else {
-                            player.sendMessage(lang.getReplacedMessage("warp-teleported").replace("%warp%", "spawn"));
+                            propermessage.send(player, lang.getReplacedMessage("warp-teleported").replace("%warp%", "spawn"));
+                            //player.sendMessage(lang.getReplacedMessage("warp-teleported").replace("%warp%", "spawn"));
 
                             String worldName = warps.getValue("spawn.location.world");
                             double x = warps.getValuedb("spawn.location.x");
