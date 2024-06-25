061feb1280ff60950469d30e37ebf2071add03b0
Awel <87609387+Invisgg@users.noreply.github.com>
2024-06-25 15:37:54 -0500

    v1.1 dev

---
+++ src/main/java/gg/minecrush/epiccore/DataStorage/ram/WarpManager.java
@@ -0,0 +1,25 @@
+package gg.minecrush.epiccore.DataStorage.ram;
+
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class WarpManager {
+
+    private final ConcurrentHashMap<UUID, Boolean>  warpingPlayers;
+
+    public WarpManager() {
+        this.warpingPlayers = new ConcurrentHashMap<UUID, Boolean>();
+    }
+
+    public void addWarp(UUID uuid) {
+        this.warpingPlayers.put(uuid, true);
+    }
+
+    public void removeWarp(UUID uuid) {
+        this.warpingPlayers.remove(uuid);
+    }
+
+    public boolean isWarping(UUID uuid) {
+        return this.warpingPlayers.getOrDefault(uuid, false);
+    }
+}
